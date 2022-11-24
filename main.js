c3ffce40fdab9216076eaff31391851d728eacce
manuel_durand <manuel.durand@mailo.com>
2022-11-24 01:09:09 +0100

    correction

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function question(){
-    let nom=prompt("quel est votre nom ? ", "écrivez ici")
+    let nom=prompt("quel est votre nom ? ", "écrivez ici");
+    let confirmation = confirm("Sûr ?");
+    if(confirmation){
+
     alert("Hello " +nom + " !");
+    }
+    else{
+        alert("erreur");
+    }
 }
